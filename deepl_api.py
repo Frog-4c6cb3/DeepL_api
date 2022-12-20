5b7e22026f6a923877650b4b81ce400871b58d2c
Gunjo Frog <frog4c6cb3@gmail.com>
2022-12-20 12:03:32 +0900

    update

--- deepl_api.py
+++ deepl_api.py
@@ -3,48 +3,77 @@ import os
 import datetime
 import csv
 import time
+import configparser
 
-# os.environ["HTTP_PROXY"] = "http://10.9.210.229:8080"
-# os.environ["HTTPS_PROXY"] = "http://10.9.210.229:8080"
-API_KEY = "b5e03c0f-1fbd-97fa-a0e8-3f59d37b7ca6:fx"
+
+config_ini = configparser.ConfigParser()
+config_ini.read('config.ini', encoding="utf-8")
+
+if config_ini.get("USER_SETTING", "use_proxy") == "yes":
+    os.environ["HTTP_PROXY"] = config_ini.get("USER_SETTING", "proxy_url")
+    os.environ["HTTPS_PROXY"] = config_ini.get("USER_SETTING", "proxy_url")
+API_KEY = config_ini.get("USER_SETTING", "deepl_api_kay")
+API_URL = config_ini.get("USER_SETTING", "deepl_api_url")
 
 
 def csv_log(source_text, target_text, target_lang, time):
-    path = os.path.join(os.path.dirname(__file__), 'log.csv')
-    with open(path, 'a', newline="") as f:
+    if config_ini.get("USER_SETTING", "log_csv_path") == "None":
+        path = "log.csv"
+    else:
+        path = os.path.join(config_ini.get(
+            "USER_SETTING", "log_csv_path"), 'log.csv')
+    with open(path, mode='a', newline="\n", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow([datetime.datetime.now(), target_lang,
                         len(source_text), time, source_text, target_text])
 
 
-def translate(source_text, target_langage):
+def translate(source_text, target_langage, split_sentences):
     # URLクエリに仕込むパラメータの辞書を作っておく
     if target_langage == "JA":
         params = {
             "auth_key": API_KEY,
             "text": source_text,
             "source_lang": 'EN',  # 入力テキストの言語を英語に設定
-            "target_lang": 'JA'  # 出力テキストの言語を日本語に設定（JPではなくJAなので注意）
+            "target_lang": 'JA',  # 出力テキストの言語を日本語に設定（JPではなくJAなので注意）
+            "preserve_formatting": "0"
         }
     else:
         params = {
             "auth_key": API_KEY,
             "text": source_text,
             "source_lang": 'JA',  # 入力テキストの言語を英語に設定
-            "target_lang": 'EN'  # 出力テキストの言語を日本語に設定（JPではなくJAなので注意）
+            "target_lang": 'EN',  # 出力テキストの言語を日本語に設定（JPではなくJAなので注意）
+            "preserve_formatting": "0"
         }
+    if split_sentences == 1:
+        params["split_sentences"] = "nonewlines"
+    else:
+        params["split_sentences"] = "1"
     start_time = time.time()
     try:
         # パラメータと一緒にPOSTする
         # free用のURL、有料版はURLが異なります
-        request = requests.post(
-            "https://api-free.deepl.com/v2/translate", data=params)
+        request = requests.post(f"https://{API_URL}/v2/translate", data=params)
         request.raise_for_status()
         result = request.json()
         elapsed_time = time.time()-start_time
         target_text = result["translations"][0]["text"]
-        csv_log(source_text, target_text, target_langage, round(elapsed_time, 5))
+        csv_log(source_text, target_text, target_langage,
+                round(elapsed_time, 5))
     except requests.exceptions.RequestException as e:
-        target_text="Error:" + str(e)
+        target_text = "Error:" + str(e)
 
     return target_text
+
+
+def usage_request():
+    usage_request = requests.post(
+        f"https://{API_URL}/v2/usage", data={"auth_key": API_KEY}
+    )
+    usage_request.raise_for_status()
+    usage_result = usage_request.json()
+    usage_result = {
+        "character_count": usage_result["character_count"],
+        "character_limit": usage_result["character_limit"]}
+    return usage_result
--- honyaku-kun.py
+++ honyaku-kun.py
@@ -1,6 +1,18 @@
 import tkinter as tk
+from tkinter.constants import RIGHT, X
 import tkinter.ttk as ttk
-from deepl_api import translate
+from deepl_api import translate, usage_request
+import pyperclip
+
+
+def indicate_limit():
+    usage_result = usage_request()
+    character_limit = usage_result["character_limit"]
+    character_count = character_limit - usage_result["character_count"]
+    limit_rate = character_count / character_limit * 100
+    limit_text = f"翻訳可能な残文字数: {round(limit_rate,1)}% "\
+        f"({character_count} / {character_limit})"
+    label1["text"] = limit_text
 
 
 def en_trans():
@@ -9,7 +21,12 @@ def en_trans():
         pass
     else:
         target_text_entry.delete(1.0, tk.END)
-        target_text_entry.insert(1.0, translate(text, "EN"))
+        if check_v.get():
+            translate_result = translate(text, "EN", 1)
+        else:
+            translate_result = translate(text, "EN", 0)
+        indicate_limit()
+        target_text_entry.insert(1.0, translate_result)
 
 
 def ja_trans():
@@ -18,7 +35,12 @@ def ja_trans():
         pass
     else:
         target_text_entry.delete(1.0, tk.END)
-        target_text_entry.insert(1.0, translate(text, "JA"))
+        if check_v.get():
+            translate_result = translate(text, "JA", 1)
+        else:
+            translate_result = translate(text, "JA", 0)
+        indicate_limit()
+        target_text_entry.insert(1.0, translate_result)
 
 
 def switch():
@@ -32,22 +54,43 @@ def delete():
     target_text_entry.delete(1.0, tk.END)
 
 
+def leftClick_1(event):
+    source_text_entry.delete(1.0, tk.END)
+    source_text_entry.insert(1.0, clip())
+
+
+def leftClick_2(event):
+    text = target_text_entry.get(1.0, "end")
+    pyperclip.copy(text)
+
+
+def clip():
+    add_clip = pyperclip.paste()
+    return add_clip
+
+
 # rootメインウィンドウの設定
 root = tk.Tk()
-root.title("翻訳くん (Ver.1.03)")
-root.geometry("750x550")
+root.title("DeepL翻訳 (Ver.4.1.0)")
+root.geometry("750x560")
+root.iconbitmap(default="icon.ico")
 
 # Frameを設定
 frame1 = ttk.Frame(root)
 frame2 = ttk.Frame(root)
 frame3 = ttk.Frame(root)
+frame4 = ttk.Frame(root)
 
 # テキストボックスwidgetを設定
 source_text_entry = tk.Text(
     frame1, height=10, width=80, relief="ridge", bd=3,)
 
+source_text_entry.bind('<Button-3>', leftClick_1)
+
 target_text_entry = tk.Text(
     frame3, height=10, width=80, relief="ridge", bd=3,)
+
+target_text_entry.bind('<Button-3>', leftClick_2)
 
 # ボタンwidgetを設定
 
@@ -56,16 +99,24 @@ button_2 = tk.Button(frame2, text="EN to JA")
 button_3 = tk.Button(frame2, text="switch")
 button_4 = tk.Button(frame2, text="delete")
 
+# チェックボックス
+check_v = tk.BooleanVar()
+chk = tk.Checkbutton(
+    frame2, text="改行を無視する", variable=check_v, onvalue=True, offvalue=False)
+
 # widgetの配置を設定
 frame1.pack(pady=5)
 frame2.pack(pady=5)
 frame3.pack(pady=5)
+frame4.pack(padx=20, pady=1, fill=X)
 source_text_entry.pack()
 target_text_entry.pack()
 button_1.grid(row=0, column=0, padx=5)
 button_2.grid(row=0, column=1, padx=5)
 button_3.grid(row=0, column=2, padx=5)
 button_4.grid(row=0, column=3, padx=5)
+chk.grid(row=0, column=4, padx=5)
+
 
 # テキストボックスのフォントを設定
 source_text_entry.configure(font=("Meiryo", 11))
@@ -75,5 +126,10 @@ button_1["command"] = en_trans
 button_2["command"] = ja_trans
 button_3["command"] = switch
 button_4["command"] = delete
+
+# ラベル
+label1 = tk.Label(frame4, text=" ", font=("Meiryo", 8))
+label1.pack(side=RIGHT)
+indicate_limit()
 
 root.mainloop()
